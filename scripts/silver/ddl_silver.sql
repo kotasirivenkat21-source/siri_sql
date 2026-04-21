362a3dc4b91e6f703a497aea9ac4a66f038b2752
KOTA SIRIVENKAT <kota.sirivenkat21@gmail.com>
2026-04-21 19:03:54 +0530

    Add DDL script for creating silver schema tables
    This script creates tables in the 'silver' schema, dropping existing tables if they already exist.

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,10 @@
+/*
+COL Script: Create Silver Tables
+
+Script Purpose:
+This script creates tables in the "silver' schema, dropping existing tables
+if they already exist.
+Run this script to re-define the DOL structure of "bronze" Tables
+
+....*/
+
